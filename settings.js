4fa91c82dc2a3cb981dac138d99e67bd7a0e1f03
Dabest312 <ijeomahdesire@gmail.com>
2025-01-19 21:15:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "263717473276"
+global.pairingNumber = "2349168783809"
 
-global.botNumber = ["263717473276"]
+global.botNumber = ["2349168783809"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
